3c59c00fa6cb7ea066529163b6d66df7524a42fd
Andres Suarez <wasb2509@gmail.com>
2025-06-13 21:12:15 -0500

    Fix baseURL para railway-firebase

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://proyecto-lsae-production.up.railway.app/api'
-  : '/api';
-
 export const api = axios.create({
-  baseURL,
+  baseURL: 'https://proyecto-lsae-production.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' }
 });
