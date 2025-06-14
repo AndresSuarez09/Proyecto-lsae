b581ea63ce9655828594d3f87425fc3f6fda3db4
Andres Suarez <wasb2509@gmail.com>
2025-06-13 20:56:25 -0500

    Fix baseURL

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://proyecto-lsae-production.up.railway.app/api'
+  : '/api';
+
 export const api = axios.create({
-  baseURL: '/api',  // Proxied to backend
+  baseURL,
   headers: { 'Content-Type': 'application/json' }
 });
