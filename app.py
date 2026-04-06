f0e7a614fffdb9f5968d953653172bc506348fac
Andres <wasb2509@gmail.com>
2026-04-05 19:11:30 -0500

    Ajustes previos a despliegue: Script-> web requirements v1.1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -7,8 +8,9 @@ from fastapi.templating import Jinja2Templates
 # Inicializamos la aplicación FastAPI
 app = FastAPI()
 
-# Indicamos dónde están las plantillas HTML
-templates = Jinja2Templates(directory="templates")
+# Ruta absoluta a la carpeta templates
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 # Ruta principal que muestra la interfaz
 @app.get("/", response_class=HTMLResponse)
